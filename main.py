4310b576a027df73dfe910dad6b48e94b204aa2d
hiphophammer <hiphophammer@hotmail.com>
2021-10-27 02:19:20 -0700

    dd

--- main.py
+++ main.py
@@ -292,7 +292,7 @@ async def quit_job(channel):
 async def wipe_channel(channel, msg = ""):
     last_msg = channel.last_message
     while last_msg is not None:
-        last_msg.delete()
+        await last_msg.delete()
         last_msg = channel.last_message
     if msg != "":
         await channel.send(msg)
