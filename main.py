aa35dc57841bc9e945da4d0d3d663fa9977bd732
hiphophammer <hiphophammer@hotmail.com>
2022-02-23 19:14:58 -0800

    added maps

--- main.py
+++ main.py
@@ -321,8 +321,11 @@ async def elden_ring(channel):
     d_tday = datetime.datetime.now()
     d1 = datetime.datetime(2022, 2, 25, hour=8)
     delta = d1 - d_tday
-    if delta.days > 0:
-        result.append('출시까지 ' + str(delta.days) + '일' + str(delta.hours % 24) + '시간' + str(delta.minutes % 60) + '분' + str(delta.seconds % 60) + '초')
+    if delta.seconds > 0:
+        hours = delta.seconds // 3600
+        minutes = (delta.seconds % 3600) // 60
+        seconds = delta.seconds % 60
+        result.append('출시까지 ' + str(delta.days) + '일' + str(hours) + '시간' + str(minutes) + '분' + str(seconds) + '초')
     z = ''.join(result)
     await channel.send(z)
 
--- test.py
+++ test.py
@@ -1,23 +1,13 @@
-import discord
-import os
+import datetime
 
-userToken = "Mjg1NjUxMTM3OTU5NTU5MTY4.WLO_Wg.tQOBIlY4j7jyXL7q3ylJhhnZJ0M"
-
-
-class chatbot(discord.Client):
-    async def on_ready(self):
-        print('Logged in')
-
-    async def on_message(self, msg):
-        if msg.content == '~add':
-            fpath = os.path.join('resources', 'wanderer_maps', '아르테미스', '로그힐')
-            file = discord.File(fpath, filename="로그힐.png")
-            await msg.channel.send("", file=file)
-            print('Done')
-
-
-
-
-
-client = chatbot()
-client.run(userToken)
+result = []
+d_tday = datetime.datetime.now()
+d1 = datetime.datetime(2022, 2, 25, hour=8)
+delta = d1 - d_tday
+if delta.seconds > 0:
+    hours = delta.seconds // 3600
+    minutes = (delta.seconds % 3600) // 60
+    seconds = delta.seconds % 60
+    result.append('출시까지 ' + str(delta.days) + '일' + str(hours) + '시간' + str(minutes) + '분' + str(seconds) + '초')
+z = ''.join(result)
+print(z)
